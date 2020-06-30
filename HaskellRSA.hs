fc098608882b848205e8b11f3c833a4f554468da
Matthew Timm <matttimm1@aol.com>
2020-06-30 12:38:39 -0600

    got the basic encryptor working

--- HaskellRSA.hs
+++ HaskellRSA.hs
@@ -1,10 +1,11 @@
+stringSizer x = if length x >= 3
+    then x
+    else stringSizer ("0" ++ x)
 convertString2Num x = 
-    let stringSizer x = if length x >= 3
-        then x
-        else stringSizer ("0" ++ x)
+    let
         convertChars2Num [] = []
         convertChars2Num (x:xs) = (stringSizer(show(fromEnum x))):(convertChars2Num xs)
-    in concat (convertChars2Num x)
+    in convertChars2Num x
 
 convertNum2String [] = []
 convertNum2String x = (toEnum (read (take 3 x) :: Int) :: Char):(convertNum2String (drop 3 x))
@@ -12,11 +13,23 @@ convertNum2String x = (toEnum (read (take 3 x) :: Int) :: Char):(convertNum2Stri
 testPrime1 = 17
 testPrime2 = 31
 
+--both of these were just practice and are in the prelude.
 gcd' x 1 = 1
 gcd' x y = if k == 0
     then y
     else gcd y (x - (y*k))
     where k = x `mod` y
 
-lcm' x y= (x*y) `div` (gcd' x y)  
+lcm' x y= (x*y) `div` (gcd' x y)
+
+encryptChunk n e m = 
+    let helper m x n 0 = x
+        helper m x n e = helper m (mod (x*m) n) n (e-1)
+    in helper (read m :: Int) 1 n e
+
+encryptMessage m n e= map (stringSizer.show) (map (encryptChunk n e) m)
+
+totientFunction x y = lcm' (x-1) (y - 1)
+
+
 
