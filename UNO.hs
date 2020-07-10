394c31ff8424f9d5d89f42d457c8a0d122fd7500
Matthew Timm <matttimm1@aol.com>
2020-07-10 08:46:17 -0600

    worked on Uno

--- UNO.hs
+++ UNO.hs
@@ -1,7 +1,7 @@
-
+import Data.List
 data Color = Red | Yellow | Blue | Green | Wild deriving (Eq, Enum, Show)
 data Action = None | Reverse| Skip | Draw2 | Draw4 deriving (Enum, Show, Eq)
-data Card = ActionCard Color Action | NumCard Color Int 
+data Card = ActionCard Color Action | NumCard Color Int deriving (Eq)
 wild = ActionCard Wild None
 wild4 = ActionCard Wild Draw4
 
@@ -20,9 +20,41 @@ createDeck =
         helper (NumCard _ _) = (take 72 (cycle [NumCard x y| x<-[(Red)..(Green)], y<-[1..9]])) ++ (helper (NumCard (Green) 10))
     in helper (NumCard (Green) 0)
 
-placeCard :: [Card] -> Card -> Bool
-placeCard _ (ActionCard Wild _ ) = True
-placeCard ((ActionCard a b):bs) (ActionCard x y) = (a == x) || (b== y)
-placeCard ((NumCard a b):bs) (NumCard x y) = (a == x) || (b== y)
-placeCard ((ActionCard a b):bs) (NumCard x y) = (a == x)
-placeCard ((NumCard a b):bs) (ActionCard x y) = (a == x) 
+canPlaceCard :: [Card] -> Card -> Bool
+canPlaceCard _ (ActionCard Wild _ ) = True
+canPlaceCard ((ActionCard Wild _):bs) _ = True
+canPlaceCard ((ActionCard a b):bs) (ActionCard x y) = (a == x) || (b== y)
+canPlaceCard ((NumCard a b):bs) (NumCard x y) = (a == x) || (b== y)
+canPlaceCard ((ActionCard a b):bs) (NumCard x y) = (a == x)
+canPlaceCard ((NumCard a b):bs) (ActionCard x y) = (a == x)
+
+drawCard hand deck = (head deck:hand, tail deck)
+
+draw2Cards hand deck =  (uncurry drawCard) (drawCard hand deck)
+
+draw4Cards hand deck = (uncurry draw2Cards) (draw2Cards hand deck)
+
+getAction (ActionCard _ y) = y
+
+unAction (ActionCard y _) = ActionCard y None
+unAction u@(NumCard y x) = u
+
+comTurn hand deck pile = 
+    let 
+        helper hand deck pile [] = ((head deck):hand,tail deck,pile, unAction (head pile), False)
+        helper hand deck pile (t:tempHand) = if canPlaceCard pile t
+            then (delete t hand, deck, t:pile, t, True)
+            else helper hand deck pile tempHand
+    in helper hand deck pile hand
+
+first5 (x,_,_,_,_) = x
+second5 (_,x,_,_,_) = x
+third5 (_,_,x,_,_) = x
+fourth5 (_,_,_,x,_) = x
+fifth5 (_,_,_,_,x) = x
+
+
+unoTurn [] _ _ _ _ _ = True
+unoTurn _ [] _ _ _ _ = False
+unoTurn p1 p2 deck pile (ActionCard color Skip) turn = unoTurn p1 p2 deck pile (ActionCard color None) (not turn)
+unoTurn p1 p2 deck pile (NumCard color number) False = unoTurn p1 (first5 k) (second5 k) (third5 k) (fourth5 k) (fifth5 k) where k = comTurn p2 deck pile
