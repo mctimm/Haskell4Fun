85b19c1cb79ad53290ddd7f699ff96332e6671de
Matthew Timm <matttimm1@aol.com>
2020-10-25 19:10:18 -0600

    Added state and started interpreter

--- BasicParser.hs
+++ BasicParser.hs
@@ -1,8 +1,12 @@
 --Basic Parser in Haskell
 import Parselib
+import Data.Array
+import System.IO
+import Data.Bits
+import Control.Monad.Trans.State.Lazy
 
-
-data Statement = Let Expr Expr | Print Expr
+data Line = Line Int Statement deriving (Show)
+data Statement = Let Expr Expr | Print Expr | END deriving (Show)
 -- data Expr = INT Int | RND Int | Var [Char] | ConstInt Int |Constr [Char] | Value Expr | PowerExpr Value PowerExpr
 --             MultExpr PowerExpr MultExpr | NegateExpr MultExpr|  AddExpr NegateExpr AddExpr| ComExpr AddExpr ComExpr|  NotExpr ComExpr| AndExpr NotExpr AndExpr| OrExpr AndExpr OrExpr
 --             | PrintList Expr PrintList | ExprList Expr ExprList 
@@ -17,25 +21,33 @@ whitespace = space
 
 driversLicense = do {c <- letter; cs <- many alphanum; return (c:cs)}
 
-wordGrabber = do {char '"'; str<- many item; token $ char '"'; return str}
+wordGrabber = do {char '\"'; str<- many $ sat (/= '\"'); token $ char '\"'; return str}
+
+statement = endStatement +++ letStatement +++ printStatement
+
+
+
+endStatement = do {x<-statementStarter "END"; return $ Line x END}
+
+
+letStatement = (do {i<-statementStarter "LET"; v<-variable; token (char '='); x<-expr; return $ Line i (Let v x) })
+
+
+printStatement = do {x<-statementStarter "PRINT"; printer <- printList; return $ Line x (Print printer)}
+
+
+
+
+variable = do {c <-token driversLicense; return $ Var c}
 
 printList = chainerFinal1 [",",";"] expr PrintList
 
 exprList = chainerFinal1 [","] expr ExprList
 
-
-
-
-
---GET THIS SHIT DONE: expr needs to defined, parses an expression
-variable = do {c <-token driversLicense; return $ Var c}
-
--- letmaker = apply (do {int; token (string "LET"); v<-variable; token (char '='); x<-expr; Let v x})
-
-value =  token (variable +++ function +++ constant +++ do {token $ char '('; e <- expr; token $ char ')'; return e})
+value =  token (variable +++ function +++ constant +++ do {token $ char '('; e <- expr; token $ char ')';return $ Value e})
 
 constant :: Parser Expr
-constant = do { x<-int; return $ ConstInt x} +++ do {str <- wordGrabber; return $ Constr str} 
+constant = do { x<-int; return $ ConstInt x} +++ do {str <-token $ wordGrabber; return $ Constr str} 
 --function needs some work
 function = do {str <-token $ (string "INT" +++ string "RND"); token $ char '('; x<- expr; token $ char ')'; return $ if str == "INT" then INT x else RND x}
 
@@ -79,3 +91,89 @@ chainerExpr c func =  token (do {token $ string c; x<-token func; return (x,c)})
 --chainerListExpr :: [[Char]] -> Parser Expr -> Parser Expr
 chainerListExpr [x] func = chainerExpr x func
 chainerListExpr (x:xs) func = token ((chainerExpr x func ) +++ (chainerListExpr xs func))
+
+statementStarter x = do {lineNumber<-int; token $ string x; return lineNumber}
+
+
+--interpreter down below
+getNumber (Line n _) = n
+getStatement (Line _ s) = s
+
+tupleListGetSnd n (x:xs) = if n == fst x 
+    then (snd x) 
+    else (tupleListGetSnd n xs)
+
+statementRenumber ls (Line n s)= Line (tupleListGetSnd n ls) s
+
+fileRenumber lsln = map (statementRenumber lsln)
+
+
+parseFile lineNumber lsln [] stateLs = listArray (0,pred lineNumber) $ reverse $ fileRenumber lsln stateLs
+parseFile lineNumber lsln input stateLs = 
+    let k = head $ apply statement input
+        newPair = (getNumber (fst k),lineNumber)
+    in parseFile (succ lineNumber) (newPair:lsln) (snd k) ((fst k):stateLs) 
+
+runSymbol "<>" x y  = fromIntegral $ (fromEnum $ (x /= y)) 
+runSymbol "<=" x y  = fromIntegral $ (fromEnum $ (x <= y)) 
+runSymbol ">=" x y  = fromIntegral $ (fromEnum $ (x >= y)) 
+runSymbol "="  x y  = fromIntegral $ (fromEnum $ (x == y)) 
+runSymbol ">"  x y  = fromIntegral $ (fromEnum $ (x > y )) 
+runSymbol "<"  x y  = fromIntegral $ (fromEnum $ (x < y )) 
+runSymbol "+"  x y  = x + y 
+runSymbol "-"  x y  = x - y 
+runSymbol "*"  x y  = x * y 
+runSymbol "/"  x y  = x `div` y 
+runSymbol "^"  x y  = x ^ y 
+runSymbol "OR" x y  = (.|.) x y  
+runSymbol "AND" x y = (.&.)  x y 
+
+--not State stuff
+--runExpr :: (Num a) => Expr -> a
+-- runExpr (MultExpr expr1 expr2 symbol)  =  (runSymbol symbol) (runExpr expr1)  (runExpr expr2)
+-- runExpr (AddExpr expr1 expr2 symbol)   =  (runSymbol symbol) (runExpr expr1)  (runExpr expr2)
+-- runExpr (PowerExpr expr1 expr2 symbol) =  (runSymbol symbol) (runExpr expr1)  (runExpr expr2)
+-- runExpr (OrExpr expr1 expr2 symbol)    =  (runSymbol symbol) (runExpr expr1)  (runExpr expr2)
+-- runExpr (AndExpr expr1 expr2 symbol)   =  (runSymbol symbol) (runExpr expr1)  (runExpr expr2)
+-- runExpr (ComExpr expr1 expr2 symbol)   =  (runSymbol symbol) (runExpr expr1)  (runExpr expr2)
+-- runExpr (NegateExpr expr) =  (-1) * (runExpr expr)
+-- runExpr (NotExpr expr) =  if (runExpr expr) == 0
+--     then 1
+--     else 0
+-- runExpr (Value expr) =  runExpr expr
+-- runExpr (ConstInt n) =  fromIntegral n
+-- runExpr (INT expr) =  runExpr expr --work on this
+-- runExpr (RND expr) =  runExpr expr
+--runExpr (Var name) = do {globalState<-get; return $ tupleListGetSnd name globalState}
+
+runExpr ::  Expr -> StateT [([Char],Expr)] IO Int
+runExpr (MultExpr expr1 expr2 symbol)  =  (runSymbol symbol) <$> (runExpr expr1) <*> (runExpr expr2)
+runExpr (AddExpr expr1 expr2 symbol)   =  (runSymbol symbol) <$> (runExpr expr1) <*> (runExpr expr2)
+runExpr (PowerExpr expr1 expr2 symbol) =  (runSymbol symbol) <$> (runExpr expr1) <*> (runExpr expr2)
+runExpr (OrExpr expr1 expr2 symbol)    =  (runSymbol symbol) <$> (runExpr expr1) <*> (runExpr expr2)
+runExpr (AndExpr expr1 expr2 symbol)   =  (runSymbol symbol) <$> (runExpr expr1) <*> (runExpr expr2)
+runExpr (ComExpr expr1 expr2 symbol)   =  (runSymbol symbol) <$> (runExpr expr1) <*> (runExpr expr2)
+runExpr (NegateExpr expr) =  ((-1) *) <$> (runExpr expr)
+runExpr (NotExpr expr) =  (\x -> if (x == 0) then 1 else 0) <$> (runExpr expr)
+runExpr (Value expr) =  runExpr expr
+runExpr (ConstInt n) =  return $ n
+runExpr (INT expr) =  runExpr expr --work on this
+runExpr (RND expr) =  runExpr expr
+runExpr (Var name) = do {globalState <- get ;runExpr (tupleListGetSnd name globalState)}
+
+
+updateState [] x = [x]
+updateState (u@(x1,y1):globalState) q@(x2,y2) = if x1 == x2
+    then (x2,y2):globalState
+    else u:(updateState globalState q)
+
+--this is the current working area
+--runStatement _ (END) = return Nothing
+--runStatement lineNumber (Let name value) = do {globalState<-get;put (updateState globalState (name,value)); return (Just (succ lineNumber))}
+--runStatement lineNumber (Print 
+
+main = do 
+    handle <- openFile "BasicFiles\\foo.bas" ReadMode
+    rawInput <- hGetContents handle
+    let input = parseFile 0 [] rawInput []
+    putStrLn $ show input
